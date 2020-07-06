af6a07a196cf81c3a6229c008aeaddb9ea5898d6
Dmitry Medvedev <office.6422609@yandex.ru>
2020-07-06 11:49:50 +0300

    feat: написать код для попапа с картинкой

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,6 @@ const popupEdit = document.querySelector('.popup_edit');
 const popupEditOpenButton = document.querySelector('.edit-button__open-popup');
 const popupEditCloseButton = popupEdit.querySelector('.popup__close');
 
-
 const profile = document.querySelector('.profile');
 const name = profile.querySelector('.profile__name');
 const occupation = profile.querySelector('.profile__occupation');
@@ -11,11 +10,11 @@ const occupation = profile.querySelector('.profile__occupation');
 const popupEditName = popupEdit.querySelector('.popup__text_name');
 const popupEditOccupation = popupEdit.querySelector('.popup__text_occupation');
 const formEditElement = popupEdit.querySelector('.popup__form_edit');
-
+// общая функция вкл-выкл попапов
 function popupToggle(popup) {
   popup.classList.toggle('popup_opened');
 }
-
+//  функция вкл-выкл попапа редактирования
 function popupEditToggle() {
   if (!popupEdit.classList.contains('popup_opened')) {
     popupEditName.value = name.textContent;
@@ -23,14 +22,14 @@ function popupEditToggle() {
   }
   popupToggle(popupEdit);
 }
-
+// функция "сохранить" попапа редактирования
 function formSubmitHandler(evt) {
   evt.preventDefault();
   name.textContent = popupEditName.value;
   occupation.textContent = popupEditOccupation.value;
   popupEditToggle();
 }
-
+// обработчики попапа редактирования
 popupEditOpenButton.addEventListener('click', popupEditToggle);
 popupEditCloseButton.addEventListener('click', popupEditToggle);
 formEditElement.addEventListener('submit', formSubmitHandler);
@@ -40,75 +39,97 @@ const popupAdd = document.querySelector('.popup_add');
 const popupAddOpenButton = document.querySelector('.add-button__open-popup');
 const popupAddCloseButton = popupAdd.querySelector('.popup__close');
 
+//  функция вкл-выкл попапа добавления карточки
 function popupAddToggle() { 
   popupToggle(popupAdd);
 }
-
+//переменные попапа добавления карточки
 const poppopupAddCardName = popupAdd.querySelector('.popup__text_cardname');
 const popupAddLink = popupAdd.querySelector('.popup__text_link') ;
 const formAddCard = popupAdd.querySelector('.popup__form_develop') ;
 
-
+// обработчики попапа добавления карточки
 popupAddOpenButton.addEventListener('click', popupAddToggle);
 popupAddCloseButton.addEventListener('click', popupAddToggle);
 
+// переменные попапа с изображением места
+const popupPlace = document.querySelector('.popup_place');
+const popupPlaceCloseButton = popupPlace.querySelector('.popup__close');
+
+//  функция вкл-выкл попапа с изображением места
+function popupPlaceToggle() { 
+  popupToggle(popupPlace);
+}
+
+// обработчик попапа с изображением места
+popupPlaceCloseButton.addEventListener('click', popupPlaceToggle);
 
 
-// создание начального блока карточек
-
+// массив начального блока карточек
 const initialCards = [
   {
-      name: 'Архыз',      
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: 'Архыз',      
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
   },
   {
-      name: 'Челябинская область',      
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: 'Челябинская область',      
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
   },
   {
-      name: 'Иваново',      
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: 'Иваново',      
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
   },
   {
-      name: 'Камчатка',      
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: 'Камчатка',      
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
   },
   {
-      name: 'Холмогорский район',      
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: 'Холмогорский район',      
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
   },
   {
-      name: 'Байкал',      
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    name: 'Байкал',      
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
 
-const allCards = document.querySelector(".cards__list");
-const cardTemplate = document.querySelector(".card-template");
+// переменныеt для создания блока карточек
+  const allCards = document.querySelector(".cards__list");
+  const cardTemplate = document.querySelector(".card-template");
+
 // функция создания блока карточек
 function addCards(item) {
   const card = cardTemplate.content.cloneNode(true);  
-  card.querySelector('.card__title').textContent = item.name;
+  card.querySelector('.card__title').textContent = item.name;  
+  card.querySelector('.card__image').src = item.link;
   card.querySelector('.card__image').alt = item.name;
-  card.querySelector('.card__image').src = item.link;    
+
   // поставить лайк карточке
   card.querySelector('.card__like').addEventListener('click', function likeCard(evt) {   
     evt.target.classList.toggle('card__like_active'); 
   });
+
     //удаление карточки  
   card.querySelector('.card__trash').addEventListener('click', function  deleteCard(evt) {
     const card = evt.target.closest('.card');
     card.remove();
   });
 
+  // попап увеличения изображения места  
+  card.querySelector('.card__image').addEventListener('click', function popupPlaceBig () {
+    popupPlace.querySelector('.popup__place-title').textContent = item.name;
+    popupPlace.querySelector('.popup__image').src = item.link;
+    popupPlaceToggle()
+  });
+  // размещаем карточку в начале блока карточек
   allCards. prepend(card);
 }
-
-initialCards.forEach(function (item) {
-  addCards(item);
+  // проходим начальный массив карточек
+  initialCards.forEach(function (item) {
+   addCards(item);
 });
 
-  // добавление новой карточки
+  // функция добавления новой карточки
 function createCard(evt) {
   evt.preventDefault();   
   const inputCardName = poppopupAddCardName.value;  
@@ -119,7 +140,5 @@ function createCard(evt) {
   addCards(inputCard);
   popupAddToggle();     
 }
-
-formAddCard .addEventListener('submit',createCard);
-
- 
+// обработчик добавления новой карточки
+formAddCard.addEventListener('submit', createCard);
