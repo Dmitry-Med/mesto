f262fa2009b9c25ec7585966cd2b7f1776e06ff6
Dmitry Medvedev <office.6422609@yandex.ru>
2020-08-18 12:43:21 +0300

    fix: исправить ошибки в коде класса PopupWithImage

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,14 +1,15 @@
 import Popup from './Popup.js';
 export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
-    super(popupSelector);    
+    super(popupSelector);
+    this._popupPlaceName =this._popupElement.querySelector('.popup__place-title');
+    this._popupPlaceImage = this._popupElement.querySelector('.popup__image');
   }
   
   open(data) {    
-    this._popupElement.querySelector('.popup__place-title').textContent = data.name;
-    const PopupPlaceImage = this._popupElement.querySelector('.popup__image');
-    PopupPlaceImage.src = data.link;
-    PopupPlaceImage.alt = data.name;     
+    this._popupPlaceName.textContent = data.name;    
+    this._popupPlaceImage.src = data.link;
+    this._popupPlaceImage.alt = data.name;     
     super.open();
   }
 }
